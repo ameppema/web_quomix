7882b5c0b1011c1b0bd16aee848124d3f365a6b7
ameppema <35190763+ameppema@users.noreply.github.com>
2025-08-06 17:58:56 +0200

    Update index.vue to reflect new branding and messaging, changing the header and introductory text to emphasize the company's focus on software and technology solutions for business success.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <UContainer class="grid grid-cols-1 md:grid-cols-2 items-center gap-12 md:gap-24 py-24 max-md:px-12 text-highlighted dark">
       <div class="max-md:order-2 flex flex-col gap-4 max-md:text-center">
-        <h1 class="text-4xl font-bold">Everything you are. In one, simple link in bio.</h1>
-        <p class="text-xl text-highlighted/75">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+        <h1 class="text-4xl font-bold">Relaciones adecuadas que potencian el éxito.</h1>
+        <p class="text-xl text-highlighted/75">Somos una empresa de software y tecnología. Trabajamos para cumplir sus objetivos empresariales orquestando el enfoque de datos, tecnología o consultoria adecuado que le ayude a conectar con sus clientes.</p>
       </div>
       <div class="max-md:order-1 flex items-center justify-center">
         <QuomixIsotype class="max-h-144 max-md:max-w-2/4" />
